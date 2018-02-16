92813f11a60533c4e36cb7d84bc239fed6b78725
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-02-16 17:46:46 +0900

    Get test account from ganache

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,21 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+let accounts
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // Use one of those accounts to deploy the contact
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts)
+  })
+})
