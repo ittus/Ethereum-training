396c84eb62549331cea9a51e84c885e9a1aaee82
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-02-16 23:24:49 +0900

    Write unit test for contract

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,20 +2,37 @@ const mocha = require('mocha');
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let accounts
+let inbox
+const INITIAL_STRING = 'Hi there!'
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts()
 
   // Use one of those accounts to deploy the contact
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000'})
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(accounts)
+    assert.ok(inbox.options.address);
+  })
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  })
+  it('can change the message', async () => {
+    const CHANGE_MESSAGE = 'Bye bye!'
+    await inbox.methods.setMessage(CHANGE_MESSAGE).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, CHANGE_MESSAGE);
   })
 })
