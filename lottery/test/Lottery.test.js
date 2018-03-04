590e5176227d2a12689f1b340f58be448aa5c37e
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-05 01:08:35 +0900

    Add lottery unit test

---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,87 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require('../compile');
+
+let accounts
+let lottery
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // Use one of those accounts to deploy the contact
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000'})
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  })
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.011', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(1, players.length);
+    assert.equal(accounts[0], players[0]);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.011', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.011', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.011', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(3, players.length);
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+  });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 10
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+})
