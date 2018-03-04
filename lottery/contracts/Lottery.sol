2988748aba60c79c0b3a033c209ea8a1e3663f99
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-04 23:50:46 +0900

    Pseudo random number

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -7,8 +7,13 @@ contract Lottery {
    function Lottery() public {
        manager = msg.sender;
    }
+
    function enter() public payable {
        require(msg.value > .01 ether);
        players.push(msg.sender);
+   }
+
+   function random() private view returns (uint) {
+       return uint(keccak256(block.difficulty, now, players));
    }
 }
