62dcf83ee316a4ad7f650d02a553576ffaf295cd
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-11 00:51:54 +0900

    Allow user to enter lottery

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends Component {
   state = {
-    manager: '',
+    manager: "",
     players: [],
-    balance: ''
+    balance: "",
+    value: "",
+    message: ''
   };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
@@ -16,12 +17,37 @@ class App extends Component {
     const balance = await web3.eth.getBalance(lottery.options.address);
     this.setState({ manager, players, balance });
   }
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction success...'});
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+    this.setState({ message: 'You have been entered!'});
+  }
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
         <p>This contract is managed by {this.state.manager}</p>
-        <p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')}</p>
+        <p>
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")}
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}/>
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
