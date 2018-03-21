1514cb27146206f8962458f24877660975cb3d33
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-21 23:41:42 +0900

    Fetch data in getInitialProps

--- kick-starter-clone/pages/index.js
+++ kick-starter-clone/pages/index.js
@@ -2,13 +2,15 @@ import factory from '../ethereum/factory';
 import React, {PropTypes} from 'react';
 
 export default class CampaignIndex extends React.Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call()
-    console.log(campaigns)
+    return {
+      campaigns
+    }
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    return (<div>{ this.props.campaigns[0] }</div>);
   }
 }
 
