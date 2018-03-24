0ebc6e7708892deaab08f8e59ff953806252fbae
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-24 09:57:02 +0900

    Add Semantic UI lib

--- kick-starter-clone/pages/index.js
+++ kick-starter-clone/pages/index.js
@@ -1,5 +1,6 @@
 import factory from '../ethereum/factory';
 import React, {PropTypes} from 'react';
+import { Card } from 'semantic-ui-react';
 
 export default class CampaignIndex extends React.Component {
   static async getInitialProps() {
@@ -9,8 +10,22 @@ export default class CampaignIndex extends React.Component {
     }
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    });
+    return <Card.Group items={items} />
+  }
+
   render() {
-    return (<div>{ this.props.campaigns[0] }</div>);
+    return (<div>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+      { this.renderCampaigns() }
+    </div>);
   }
 }
 
