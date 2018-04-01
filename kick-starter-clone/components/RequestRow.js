0474be6fba40dbf33725ac5587c7daacf4aa04bd
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-04-01 20:48:28 +0900

    Implement approve and finalize function

--- kick-starter-clone/components/RequestRow.js
+++ kick-starter-clone/components/RequestRow.js
@@ -1,13 +1,31 @@
 import React, {PropTypes} from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
 export default class RequestRow extends React.Component {
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  }
+
+  onFinalize = async () => {
+    const campaign = Campaign(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  }
+
   render() {
     const { Row, Cell } = Table;
-    const { id, request } = this.props;
+    const { id, request, approversCount } = this.props;
+    const readyToFinalize = request.approvalCount > approversCount * 0.5;
     return (
-      <Row>
+      <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
         <Cell>
           {id}
         </Cell>
@@ -24,10 +42,18 @@ export default class RequestRow extends React.Component {
           {request.approvalCount}/{this.props.approversCount}
         </Cell>
         <Cell>
-          Approve
+          {
+            request.complete ? null : (
+              <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+            )
+          }
         </Cell>
         <Cell>
-          Finalize
+          {
+              request.complete ? null : (
+                <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+              )
+          }
         </Cell>
       </Row>
     );
